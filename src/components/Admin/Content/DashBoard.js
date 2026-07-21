ea73ea6075f6412b60913d42b4d0e7cd1b5b032d
Sang Vương <vuonghoaisang@gmail.com>
2026-07-21 10:03:16 +0700

    Sap hoan thien p2

--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -11,10 +11,11 @@ import {
 } from "recharts";
 import { getOverView } from "../../../services/apiServices";
 import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const DashBoard = (props) => {
-  const [dataOverView, setDataOverView] = useState([]);
-  const [dataChart, setDataChart] = useState([]);
+  const [dataOverView, setDataOverView] = useState({});
+  const { t } = useTranslation();
   useEffect(() => {
     fetchDataOverView();
   }, []);
@@ -22,89 +23,55 @@ const DashBoard = (props) => {
     let res = await getOverView();
     if (res && res.EC === 0) {
       setDataOverView(res.DT);
-      //process chart data
-      let Qz = 0,
-        Qs = 0,
-        As = 0;
-      Qz = res?.DT?.others?.countQuiz ?? 0;
-      Qs = res?.DT?.others?.countQuestions ?? 0;
-      As = res?.DT?.others?.countAnswers ?? 0;
-      const data = [
-        {
-          name: "Quizzes",
-          Qz: Qz,
-        },
-        {
-          name: "Questions",
-          Qs: Qs,
-        },
-        {
-          name: "Answers",
-          As: As,
-        },
-      ];
-      setDataChart(data);
     }
     console.log("Check res: ", res);
   };
-
+  // data chart tạo lại mỗi lần render
+  const dataChart = [
+    {
+      name: t("dashboard.data.name1"),
+      Qz: dataOverView?.others?.countQuiz ?? 0,
+    },
+    {
+      name: t("dashboard.data.name2"),
+      Qs: dataOverView?.others?.countQuestions ?? 0,
+    },
+    {
+      name: t("dashboard.data.name3"),
+      As: dataOverView?.others?.countAnswers ?? 0,
+    },
+  ];
   console.log("Check dataOverView: ", dataOverView);
   return (
     <div className="dashboard-container">
-      <div className="title">Analytics Dashboard</div>
+      <div className="title">{t("dashboard.title")}</div>
       <div className="content">
         <div className="c-left">
           <div className="child">
-            <span className="text-1">Total users</span>
+            <span className="text-1">{t("dashboard.users")}</span>
+            <span className="text-2">{dataOverView?.users?.total ?? 0}</span>
+          </div>
+          <div className="child">
+            <span className="text-1">{t("dashboard.quizzes")}</span>
             <span className="text-2">
-              {dataOverView &&
-              dataOverView.users &&
-              dataOverView.users.total ? (
-                <>{dataOverView.users.total}</>
-              ) : (
-                <>0</>
-              )}
+              {dataOverView?.others?.countQuiz ?? 0}
             </span>
           </div>
           <div className="child">
-            <span className="text-1"> Total Quizzes</span>
+            <span className="text-1">{t("dashboard.questions")}</span>
             <span className="text-2">
-              {dataOverView &&
-              dataOverView.others &&
-              dataOverView.others.countQuiz ? (
-                <>{dataOverView.others.countQuiz}</>
-              ) : (
-                <>0</>
-              )}
+              {dataOverView?.others?.countQuestions ?? 0}
             </span>
           </div>
           <div className="child">
-            <span className="text-1">Total Questions</span>
+            <span className="text-1">{t("dashboard.answers")}</span>
             <span className="text-2">
-              {dataOverView &&
-              dataOverView.others &&
-              dataOverView.others.countQuestions ? (
-                <>{dataOverView.others.countQuestions}</>
-              ) : (
-                <>0</>
-              )}
-            </span>
-          </div>
-          <div className="child">
-            <span className="text-1"> Total Answers</span>
-            <span className="text-2">
-              {dataOverView &&
-              dataOverView.others &&
-              dataOverView.others.countAnswers ? (
-                <>{dataOverView.others.countAnswers}</>
-              ) : (
-                <>0</>
-              )}
+              {dataOverView?.others?.countAnswers ?? 0}
             </span>
           </div>
         </div>
         <div className="c-right">
-          <ResponsiveContainer width="100%" height={"100%"}>
+          <ResponsiveContainer width="100%" height="100%">
             <BarChart data={dataChart}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="name" />
